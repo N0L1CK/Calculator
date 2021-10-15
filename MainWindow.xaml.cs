fefc5d367c59a8677ef0bc905bc0dff53f98dea8
EVGENII OBEDINSKII <obe.work@gmail.com>
2021-10-15 11:16:32 +0700

    //TODO: something wrong whith point, i dont no  how fix that. bugpoint :^)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,10 +20,13 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double FerstOperand;
-        //private readonly double SecondOperand;
-        //private readonly double Result;
-        //private readonly string SignOperation;
+        
+        private double firstOperand;
+        private double secondOperand;
+        private string sighn;
+        private bool mathRes;
+        private bool havePoint;
+    
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +35,7 @@ namespace Calculator
         private void ButtonClickNumber(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
+
             switch (button.Content) 
             {
                 case "1":
@@ -43,8 +47,11 @@ namespace Calculator
                 case "7":
                 case "8":
                 case "9":
-                    if (textBlock.Text == "0")
+                    if (textBlock.Text == "0" || mathRes)
+                    {
                         textBlock.Text = button.Content.ToString();
+                        mathRes = false;
+                    }
                     else
                         textBlock.Text += button.Content;
                     break;
@@ -55,52 +62,138 @@ namespace Calculator
                         textBlock.Text += button.Content;
                     break;
 
+                   
             }
         }
         private void ButtonClickSign(object sender, RoutedEventArgs e)
         {
+            if (textBlock.Text != "0" ) 
+            {
+                if (Convert.ToDouble(textBlock.Text) > 0)
+                {
+                    textSign.Text = "-";
+                    
+                }
+                else
+                {
+                    textSign.Text = "";
 
+                }
+            }
         }
 
         private void ButtonClickOperation(object sender, RoutedEventArgs e)
         {
             Button button = (Button)sender;
-            FerstOperand = Convert.ToDouble(textBlock.Text);
-            
+            if (textSign.Text == "-")
+            {
+                firstOperand = Convert.ToDouble(textBlock.Text) * -1;
+            }
+            else
+                firstOperand = Convert.ToDouble(textBlock.Text);
+
+
+
+            mathRes = true;
+            havePoint = false;
+            textBlock.Text = "0";
+            textSign.Text = "";
             switch (button.Content) 
             {
                 case "+":
                 case "-":
                 case "*":
-                case "/":
-                case "√x":
-                case "X^2":
+                case "/":    
                 case "%":
-                    textBlockRes.Text += $" {textBlock.Text} {button.Content}";
-                    textBlock.Text = "0";
+                    sighn = button.Content.ToString();
                     break;
 
             }
         }
         private void ButtonClickEqls(object sender, RoutedEventArgs e)
         {
-
+            textSign.Text = "";
+            if (textSign.Text == "-")
+            {
+                secondOperand = Convert.ToDouble(textBlock.Text) * -1;
+            }
+            else
+                secondOperand = Convert.ToDouble(textBlock.Text);
+            mathRes = true;
+            
+            switch (sighn)
+            {
+                case "+": 
+                    textBlock.Text = SignRelise(firstOperand + secondOperand);
+                    break;
+                case "-":
+                    textBlock.Text = SignRelise(firstOperand - secondOperand);
+                    break;
+                case "*":
+                    textBlock.Text = SignRelise(firstOperand * secondOperand);
+                    break;
+                case "/":
+                    if (firstOperand != 0 && secondOperand != 0)
+                    {
+                        textBlock.Text = SignRelise(firstOperand * secondOperand);
+                        break;
+                    }
+                    else
+                        textBlock.Text = "0";
+                    break;
+                case "%":
+                    break;
+            }
+            firstOperand = 0;
+            secondOperand = 0;
 
         }
         private void ButtonClickPoint(object sender, RoutedEventArgs e)
         {
-
-        }
-        private void ButtonClickCE(object sender, RoutedEventArgs e) 
-        {
-            textBlock.Text = "0";
+            Button button = (Button)sender;
+            if (textBlock.Text == "0")
+            {
+                textBlock.Text += button.Content.ToString();
+                havePoint = true;
+            }
+            else if (!havePoint) 
+            {
+                textBlock.Text += button.Content.ToString();
+                havePoint = true;
+            }
         }
         private void ButtonClickC(object sender, RoutedEventArgs e) 
         {
             textBlock.Text = "0";
-            textBlockRes.Text = "";
+            textSign.Text = "";
+            havePoint = false;
+
         }
-        private void ButtonClickOpenBracket(object sender, RoutedEventArgs e) { }
-        private void ButtonClickCloseBracket(object sender, RoutedEventArgs e) { }
+        private void ButtonClickMath(object sender, RoutedEventArgs e)
+        {
+            havePoint = false;
+            mathRes = true;
+            Button button = (Button)sender;
+            if (button.Content.ToString() == "X^2")
+            {
+                firstOperand = Convert.ToDouble(textBlock.Text);
+                textBlock.Text = Convert.ToString(Math.Pow(firstOperand, 2));
+            }
+            if (button.Content.ToString() == "√x") 
+            {
+                firstOperand = Convert.ToDouble(textBlock.Text);
+                textBlock.Text = Convert.ToString(Math.Sqrt(firstOperand));
+            }
+        }
+        private string SignRelise(double digit) 
+        {
+            if (digit < 0)
+            {
+                textSign.Text = "-";
+                return (digit * -1).ToString();
+            }
+            else
+                return digit.ToString();
+        }
     }
 }
